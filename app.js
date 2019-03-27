efd9b3509030a617c80d74768647f35752b2ba23
Steve Cattanach <stevecatt@att.net>
2019-03-27 15:01:59 -0500

    saved app.js

--- app.js
+++ app.js
@@ -16,41 +16,7 @@ app.use(bodyParser.json())
 app.use('/css',express.static('css'))
 app.use('/',movieRoutes)
 
-/*let movieslist=[]
 
-app.get('/movies',(req,res)=>{
-
-    res.render ("movies",{movies:movieslist})
-})
-app.post('/movies',(req,res)=>{
-    res.redirect('/movies')
-})
-
-app.post('/movies/add',(req,res)=>{
-    let title= req.body.title
-    let description=req.body.description
-    let genre = req.body.genre
-    let imageUrl = req.body.imageUrl
-    let id=uuidv1()
-
-    let movie = {id:id,title:title,description:description,genre:genre,imageUrl:imageUrl}
-    movieslist.push(movie)
-    
-    res.redirect('/movies')
-})
-
-app.post('/movies/delete',(req,res)=>{
-    let id=req.body.id
-    console.log(id)
-    movieslist = movieslist.filter(movie => movie.id != id)
-        
-    res.redirect("/movies")
-        
-    
-})
-
-
-*/
 app.listen(3000,()=>{
     console.log("At your service")
 })
